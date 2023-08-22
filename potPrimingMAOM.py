c78b0418e42a9cfc9bd3ed795d86a8f2d5caba62
Olga Vindušková <olga.vinduskova@gmail.com>
2023-08-22 15:29:03 +0200

    added a new function Dailyplot
    the function plots respiration (distinguished into soil-derived and substrate-derived) and microbial biomass

--- potPrimingMAOM.py
+++ potPrimingMAOM.py
@@ -7,6 +7,7 @@ Created on Fri Aug 18 15:08:20 2023
 import keylink_functions as mf
 import matplotlib.pyplot as plt
 
+time_d=[]
 outMAOM=[]
 outMAOMsaturation=[]
 outDOM=[]
@@ -46,6 +47,7 @@ SOM=SOMini
 MAOM=MAOMini
 
 for d in range(numDays):
+      time_d.append(d)  #store days in an array for plotting
       DOM_N=DOM_RS/CN_DOM_RS
       if (d%14)==0:
           DOM_RS+=DOMinput/CN_DOM_RS
@@ -64,7 +66,31 @@ for d in range(numDays):
 # plt.plot(outMAOM)      
 # plt.plot (outDOM)
 # plt.plot (outMAOMsaturation) 
-plt.plot(outBact_RS)   
+# plt.plot(outBact_RS)   
 # plt.plot(outPRIMING) 
-plt.plot(outResp)
-plt.plot(outRespPriming)   
+# plt.plot(outResp)
+# plt.plot(outRespPriming)   
+
+
+def Dailyplot(outBact_RS,outResp, outRespPriming):
+    # df2 = pd.DataFrame(df)
+    # x = []
+    # y = []
+    fig, (p1, p2) = plt.subplots(nrows=2,
+                                                                                      ncols=1,
+                                                                                      figsize=(10, 12))
+    fig.tight_layout(pad=2.0)
+    ps = (p1, p2)
+    # counter = count(0, 1)
+    # columns = list(df)
+    ps[0].set_title("Respiration, gC-CO2 g-1 h-1")
+    ps[1].set_title("Microbial biomass, gC m-3")
+   
+    p1.plot(time_d, outResp, label="substrate-derived")
+    p1.plot(time_d, outRespPriming, label="soil-derived")
+    ps[0].legend(loc=(1.01, 0), shadow=True) #loc='upper right',
+
+    p2.plot(time_d, outBact_RS, label="bacterial biomass")
+    # plt.legend(loc=(1.01, 0), shadow=True) #loc='upper right',
+
+Dailyplot(outBact_RS,outResp, outRespPriming)
