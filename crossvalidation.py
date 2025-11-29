a6af5ddbb181d962806914024a0be90ce45375ff
Olga Vindušková <olga.vinduskova@gmail.com>
2025-11-29 12:50:25 +0100

    preparation for running kfold at work from crossvalidation.py

---
+++ crossvalidation.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 29 12:26:27 2025
+
+@author: Olga
+"""
+#running with second option parameter set, keeping all
+import runpy
+
+def run_with_path(path_value):
+    runpy.run_path("Potprim_main.py", init_globals={"path_bayesian": path_value})
+
+paths = [
+    "./input files crossvalidation 2022/calibration/Bayesian_run_input_2022_subset1.csv",
+    "./input files crossvalidation 2022/calibration/Bayesian_run_input_2022_subset2.csv"
+    # "./input files crossvalidation 2022/calibration/Bayesian_run_input_2022_subset3.csv",
+    # "./input files crossvalidation 2022/calibration/Bayesian_run_input_2022_subset4.csv",
+    # "./input files crossvalidation 2022/calibration/Bayesian_run_input_2022_subset5.csv"
+    
+]
+
+for p in paths:
+    print("************************************************************************")
+    print("running for:")
+    print(p)
+    print("************************************************************************")
+    run_with_path(p)
